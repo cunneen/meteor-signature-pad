12fac8fed533377b86907a8ecb5171cfcac21447
Mike Cunneen <mike@cunneen.biz>
2015-02-08 18:52:56 +0800

    signaturePad for meteor.

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "Wrapper for signature-pad https://github.com/szimek/signature_pad",
+    version: '1.0.0',
+    name: "cunneen:signature-pad",
+    githubUrl: 'https://github.com/cunneen/meteor-signature-pad/'
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('lib/signature_pad.js', 'client');
+  api.export('SignaturePad');
+});
+
